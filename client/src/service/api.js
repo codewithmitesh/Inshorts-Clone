cf7659b11c055302e9bd5726eba2c6ed9340aa7b
Mitesh Tank <codewithmitesh@gmail.com>
2022-10-10 09:30:12 +0530

    Deployment

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
-// const URL = '';
+// const URL = 'http://localhost:8000';
+const URL = '';
 
 // requesting data from the server
 export const getNews = async () => {
